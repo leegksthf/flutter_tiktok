c6a8455c3f1f3de8b21e2371d37cb57989af1ba5
이한솔 <leecldy@naver.com>
2023-07-14 17:44:58 +0900

    make customScrollView

--- lib/features/users/user_profile_screen.dart
+++ lib/features/users/user_profile_screen.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
 
 class UserProfileScreen extends StatefulWidget {
   const UserProfileScreen({super.key});
@@ -12,6 +10,43 @@ class UserProfileScreen extends StatefulWidget {
 class _UserProfileScreenState extends State<UserProfileScreen> {
   @override
   Widget build(BuildContext context) {
-    return const CustomScrollView();
+    return CustomScrollView(
+      slivers: [
+        SliverAppBar(
+          snap: true,
+          floating: true,
+          stretch: true,
+          backgroundColor: Colors.teal,
+          collapsedHeight: 200,
+          expandedHeight: 200,
+          flexibleSpace: FlexibleSpaceBar(
+            stretchModes: const [
+              StretchMode.blurBackground,
+              StretchMode.zoomBackground,
+            ],
+            background: Image.asset(
+              'assets/images/dami2.jpeg',
+              fit: BoxFit.cover,
+            ),
+            title: const Text('Hello!'),
+          ),
+        ),
+        SliverFixedExtentList(
+          delegate: SliverChildBuilderDelegate(
+            childCount: 50,
+            (context, index) => Container(
+              color: Colors.amber[100 * (index % 9)],
+              child: Align(
+                alignment: Alignment.center,
+                child: Text(
+                  'Item $index',
+                ),
+              ),
+            ),
+          ),
+          itemExtent: 100,
+        )
+      ],
+    );
   }
 }
