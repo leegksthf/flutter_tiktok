b3b650f416e6a5c436118a569589e87f98882950
이한솔 <leecldy@naver.com>
2023-07-17 11:10:57 +0900

    change tabbar to sliver persistent header in profile section

---
+++ lib/features/users/widgets/persistent_tab_bar.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:tiktok_clone/constants/sizes.dart';
+
+class PersistentTabBar extends SliverPersistentHeaderDelegate {
+  @override
+  Widget build(
+      BuildContext context, double shrinkOffset, bool overlapsContent) {
+    return Container(
+      padding: const EdgeInsets.only(
+        top: Sizes.size10,
+      ),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        border: Border.symmetric(
+          horizontal: BorderSide(
+            color: Colors.grey.shade200,
+          ),
+        ),
+      ),
+      child: const TabBar(
+        indicatorSize: TabBarIndicatorSize.label,
+        indicatorColor: Colors.black,
+        labelPadding: EdgeInsets.only(
+          bottom: Sizes.size10,
+        ),
+        tabs: [
+          Padding(
+            padding: EdgeInsets.symmetric(
+              horizontal: Sizes.size20,
+            ),
+            child: Icon(
+              Icons.grid_4x4_outlined,
+              color: Colors.black,
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.symmetric(
+              horizontal: Sizes.size20,
+            ),
+            child: FaIcon(
+              FontAwesomeIcons.heart,
+              color: Colors.black,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  @override
+  double get maxExtent => 47;
+
+  @override
+  double get minExtent => 47;
+
+  @override
+  bool shouldRebuild(covariant SliverPersistentHeaderDelegate oldDelegate) {
+    return false;
+  }
+}
